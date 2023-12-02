1c1d4a6165c6b5485abf430c41cf801ef91cc07c
galusmarcin87 <galusmarcin87@gmail.com>
2023-12-02 19:19:39 +0100

    q

--- modules/backend/views/mgcms/payment/index.php
+++ modules/backend/views/mgcms/payment/index.php
@@ -1,7 +1,9 @@
 <?php
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\mgcms\db\PaymentSearch */
+
 /* @var $dataProvider yii\data\ActiveDataProvider */
+
 use yii\helpers\Html;
 use kartik\export\ExportMenu;
 use kartik\grid\GridView;
@@ -18,11 +20,11 @@ $this->registerJs($search);
 ?>
 <div class="payment-index">
 
-  <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($this->title) ?></h1>
 
-  <?php
-  $gridColumn = [
-      ['class' => 'yii\grid\SerialColumn'],
+    <?php
+    $gridColumn = [
+        ['class' => 'yii\grid\SerialColumn'],
 //        [
 //            'class' => 'kartik\grid\ExpandRowColumn',
 //            'width' => '50px',
@@ -35,66 +37,66 @@ $this->registerJs($search);
 //            'headerOptions' => ['class' => 'kartik-sheet-style'],
 //            'expandOneOnly' => true
 //        ],
-      ['attribute' => 'id', 'visible' => false],
-      'created_on',
-      [
-          'attribute' => 'user_id',
-          'label' => Yii::t('app', 'User'),
-          'value' => function($model) {
-            return $model->user->username;
-          },
-          'filterType' => GridView::FILTER_SELECT2,
-          'filter' => \yii\helpers\ArrayHelper::map(\app\models\mgcms\db\User::find()->asArray()->all(), 'id', 'username'),
-          'filterWidgetOptions' => [
-              'pluginOptions' => ['allowClear' => true],
-          ],
-          'filterInputOptions' => ['placeholder' => Yii::t('app', 'User'), 'id' => 'grid-payment-search-user_id']
-      ],
+        ['attribute' => 'id', 'visible' => false],
+        'created_on',
+        [
+            'attribute' => 'user_id',
+            'label' => Yii::t('app', 'User'),
+            'value' => function ($model) {
+                return $model->user->username;
+            },
+            'filterType' => GridView::FILTER_SELECT2,
+            'filter' => \yii\helpers\ArrayHelper::map(\app\models\mgcms\db\User::find()->asArray()->all(), 'id', 'username'),
+            'filterWidgetOptions' => [
+                'pluginOptions' => ['allowClear' => true],
+            ],
+            'filterInputOptions' => ['placeholder' => Yii::t('app', 'User'), 'id' => 'grid-payment-search-user_id']
+        ],
+        'status',
+        'amount',
+        'project',
+        [
+            'class' => app\components\mgcms\yii\ActionColumn::className(),
+            'template' => '{view} {delete}',
+            'buttons' => [
+                'save-as-new' => function ($url) {
+                    return Html::a('<span class="glyphicon glyphicon-copy"></span>', $url, ['title' => 'Save As New']);
+                },
+            ],
+        ],
+    ];
 
-      'amount',
-      'project',
-      [
-          'class' => app\components\mgcms\yii\ActionColumn::className(),
-          'template' => '{view} {delete}',
-          'buttons' => [
-              'save-as-new' => function ($url) {
-                return Html::a('<span class="glyphicon glyphicon-copy"></span>', $url, ['title' => 'Save As New']);
-              },
-          ],
-      ],
-  ];
+    ?>
+    <?=
+    GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => $gridColumn,
+        'pjax' => true,
+        'pjaxSettings' => ['options' => ['id' => 'kv-pjax-container-payment']],
+        'panel' => [
+            'type' => GridView::TYPE_PRIMARY,
+            'heading' => '<span class="glyphicon glyphicon-book"></span>  ' . Html::encode($this->title),
+        ],
+        // your toolbar can include the additional full export menu
+        'toolbar' => [
+            '{export}',
+            ExportMenu::widget([
+                'dataProvider' => $dataProvider,
+                'columns' => $gridColumn,
+                'target' => ExportMenu::TARGET_BLANK,
+                'fontAwesome' => true,
+                'dropdownOptions' => [
+                    'label' => 'Full',
+                    'class' => 'btn btn-default',
+                    'itemsBefore' => [
+                        '<li class="dropdown-header">Export All Data</li>',
+                    ],
+                ],
+            ]),
+        ],
+    ]);
 
-  ?>
-  <?=
-  GridView::widget([
-      'dataProvider' => $dataProvider,
-      'filterModel' => $searchModel,
-      'columns' => $gridColumn,
-      'pjax' => true,
-      'pjaxSettings' => ['options' => ['id' => 'kv-pjax-container-payment']],
-      'panel' => [
-          'type' => GridView::TYPE_PRIMARY,
-          'heading' => '<span class="glyphicon glyphicon-book"></span>  ' . Html::encode($this->title),
-      ],
-      // your toolbar can include the additional full export menu
-      'toolbar' => [
-          '{export}',
-          ExportMenu::widget([
-              'dataProvider' => $dataProvider,
-              'columns' => $gridColumn,
-              'target' => ExportMenu::TARGET_BLANK,
-              'fontAwesome' => true,
-              'dropdownOptions' => [
-                  'label' => 'Full',
-                  'class' => 'btn btn-default',
-                  'itemsBefore' => [
-                      '<li class="dropdown-header">Export All Data</li>',
-                  ],
-              ],
-          ]),
-      ],
-  ]);
-
-  ?>
+    ?>
 
 </div>
