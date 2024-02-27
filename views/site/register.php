fb6db01bc169d3db640677c2cb136f600a108c52
galusmarcin87 <galusmarcin87@gmail.com>
2024-02-27 20:52:59 +0100

    mzielinska 1206702692338111

--- views/site/register.php
+++ views/site/register.php
@@ -16,9 +16,8 @@ $this->title = Yii::t('db', 'Register');
 
 <div class="container">
 
-
     <div class="text-center my-5">
-        <img src="/images/logo.png" alt="Opal Crowd" class="img-fluid mx-auto">
+        <img src="/images/logo.png" alt="ECM" class="img-fluid mx-auto">
     </div>
 
     <h3 class="my-5 text-center">
@@ -35,10 +34,9 @@ $this->title = Yii::t('db', 'Register');
     echo $form->errorSummary($model);
     ?>
     <div class="row">
-        <div class="col-lg-6 mx-auto">
-
-
-            <div class="text-center mb-4">
+        <div class="col-lg-8 mx-auto">
+			
+            <div class="text-center mb-3">
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="RegisterForm[isCompany]" id="client_type-person"
                            checked
@@ -50,90 +48,85 @@ $this->title = Yii::t('db', 'Register');
                            value="1">
                     <label class="form-check-label" for="client_type-company"><?= Yii::t('db', 'company') ?></label>
                 </div>
-
             </div>
-
-            <div class="mb-4">
-                <?= $form->field($model, 'username')->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
-            </div>
-
-            <div class="mb-4">
-                <?= $form->field($model, 'password')->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
-            </div>
-
-            <div class="mb-4">
-                <?= $form->field($model, 'passwordRepeat')->passwordInput(['placeholder' => $model->getAttributeLabel('passwordRepeat')]) ?>
-            </div>
-
-
-            <div class="mb-4">
-                <?= $form->field($model, 'firstName')->textInput(['placeholder' => $model->getAttributeLabel('firstName')]) ?>
-            </div>
-
-
-            <div class="mb-4">
-                <?= $form->field($model, 'surname')->textInput(['placeholder' => $model->getAttributeLabel('surname')]) ?>
-            </div>
-
-
-            <div class="mb-4">
-                <?= $form->field($model, 'birthDate')->textInput(["onfocus" => "(this.type='date')", 'placeholder' => $model->getAttributeLabel('birthDate')]) ?>
-            </div>
-
-
-            <div class="mb-4">
-                <?= $form->field($model, 'street')->textInput(['placeholder' => $model->getAttributeLabel('street')]) ?>
-            </div>
-
-
-            <div class="mb-4">
-                <?= $form->field($model, 'houseNo')->textInput(['placeholder' => $model->getAttributeLabel('houseNo')]) ?>
-            </div>
-
-
-            <div class="mb-4">
-                <?= $form->field($model, 'flatNo')->textInput(['placeholder' => $model->getAttributeLabel('flatNo')]) ?>
-            </div>
-
-
-            <div class="mb-4">
-                <?= $form->field($model, 'postalCode')->textInput(['placeholder' => $model->getAttributeLabel('postalCode')]) ?>
-            </div>
-
-
-            <div class="mb-4">
-                <?= $form->field($model, 'city')->textInput(['placeholder' => $model->getAttributeLabel('city')]) ?>
-            </div>
-
-
-            <div class="mb-4">
-                <?= $form->field($model, 'voivodeship')->textInput(['placeholder' => $model->getAttributeLabel('voivodeship')]) ?>
-            </div>
-
-
-            <div class="mb-4 companyFields hidden">
-                <?= $form->field($model, 'nip')->textInput(['placeholder' => $model->getAttributeLabel('nip')]) ?>
-            </div>
-
-            <div class="mb-4 companyFields hidden">
-                <?= $form->field($model, 'regon')->textInput(['placeholder' => $model->getAttributeLabel('regon')]) ?>
-            </div>
-
-
-            <div class="mb-4">
-                <?= $form->field($model, 'pesel')->textInput(['placeholder' => $model->getAttributeLabel('pesel')]) ?>
-            </div>
-
-
-            <div class="mb-4">
-                <?= $form->field($model, 'idNumber')->textInput(['placeholder' => $model->getAttributeLabel('idNumber')]) ?>
-            </div>
-
-
+            <div class="mb-3">
+				<?= $form->field($model, 'username')->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
+			</div>
+			<div class="mb-3">
+				<?= $form->field($model, 'password')->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
+			</div>
+			<div class="mb-3">
+				<?= $form->field($model, 'passwordRepeat')->passwordInput(['placeholder' => $model->getAttributeLabel('passwordRepeat')]) ?>
+			</div>
+			<div class="mb-3 companyFields hidden">
+				<?= $form->field($model, 'company_name')->textInput(['placeholder' => $model->getAttributeLabel('company_name')]) ?>
+			</div>
+			<div class="mb-3 companyFields hidden">
+				<?= $form->field($model, 'company_street')->textInput(['placeholder' => $model->getAttributeLabel('company_street')]) ?>
+			</div>
+			<div class="mb-3 companyFields hidden">
+				<?= $form->field($model, 'company_house_no')->textInput(['placeholder' => $model->getAttributeLabel('company_house_no')]) ?>
+			</div>
+			<div class="mb-3 companyFields hidden">
+				<?= $form->field($model, 'company_flat_no')->textInput(['placeholder' => $model->getAttributeLabel('company_flat_no')]) ?>
+			</div>
+			<div class="mb-3 companyFields hidden">
+				<?= $form->field($model, 'company_postcode')->textInput(['placeholder' => $model->getAttributeLabel('company_postcode')]) ?>
+			</div>
+			<div class="mb-3 companyFields hidden">
+				<?= $form->field($model, 'company_city')->textInput(['placeholder' => $model->getAttributeLabel('company_city')]) ?>
+			</div>
+			<div class="mb-3 companyFields hidden">
+				<?= $form->field($model, 'company_country')->textInput(['placeholder' => $model->getAttributeLabel('company_country')]) ?>
+			</div>
+			<div class="mb-3 companyFields hidden">
+				<?= $form->field($model, 'company_nip')->textInput(['placeholder' => $model->getAttributeLabel('company_nip')]) ?>
+			</div>
+			<div class="mb-3 companyFields hidden">
+				<?= $form->field($model, 'company_regon')->textInput(['placeholder' => $model->getAttributeLabel('company_regon')]) ?>
+			</div>
+			<div class="mb-3 companyFields hidden">
+				<?= $form->field($model, 'company_krs')->textInput(['placeholder' => $model->getAttributeLabel('company_krs')]) ?>
+			</div>
+            <div class="mb-3">
+				<?= $form->field($model, 'first_name')->textInput(['placeholder' => $model->getAttributeLabel('first_name')]) ?>
+			</div>
+			<div class="mb-3">
+				<?= $form->field($model, 'last_name')->textInput(['placeholder' => $model->getAttributeLabel('last_name')]) ?>
+			</div>
+            <div class="mb-3">
+				<?= $form->field($model, 'birthDate')->textInput(["onfocus" => "(this.type='date')", 'placeholder' => $model->getAttributeLabel('birthDate')]) ?>
+			</div>
+            <div class="mb-3">
+				<?= $form->field($model, 'street')->textInput(['placeholder' => $model->getAttributeLabel('street')]) ?>
+			</div>
+            <div class="mb-3">
+				<?= $form->field($model, 'house_no')->textInput(['placeholder' => $model->getAttributeLabel('house_no')]) ?>
+			</div>
+            <div class="mb-3">
+				<?= $form->field($model, 'flat_no')->textInput(['placeholder' => $model->getAttributeLabel('flat_no')]) ?>
+			</div>
+            <div class="mb-3">
+				<?= $form->field($model, 'postcode')->textInput(['placeholder' => $model->getAttributeLabel('postcode')]) ?>
+			</div>
+            <div class="mb-3">
+				<?= $form->field($model, 'city')->textInput(['placeholder' => $model->getAttributeLabel('city')]) ?>
+			</div>
+			<div class="mb-3">
+				<?= $form->field($model, 'country')->textInput(['placeholder' => $model->getAttributeLabel('country')]) ?>
+			</div>
+            <div class="mb-3">
+				<?= $form->field($model, 'voivodeship')->textInput(['placeholder' => $model->getAttributeLabel('voivodeship')]) ?>
+			</div>
+            <div class="mb-3">
+				<?= $form->field($model, 'pesel')->textInput(['placeholder' => $model->getAttributeLabel('pesel')]) ?>
+			</div>
+            <div class="mb-3">
+				<?= $form->field($model, 'id_document_no')->textInput(['placeholder' => $model->getAttributeLabel('id_document_no')]) ?>
+			</div>
         </div>
-
     </div>
-
+	
     <div class="row">
         <div class="col-lg-9 mx-auto">
             <div class="text-center">
@@ -176,14 +169,44 @@ $this->title = Yii::t('db', 'Register');
 
 
     <?php ActiveForm::end(); ?>
-    <script>
-        $('input[type=radio][name="RegisterForm[isCompany]"').change(function() {
-            if(this.value == 1){
-                $('.companyFields').removeClass('hidden');
-            }else{
-                $('.companyFields').addClass('hidden');
-            }
-        });
-    </script>
+
+<script>
+$(document).ready(function() {
+    // Ukryj lub pokaż pola na podstawie aktualnie zaznaczonej wartości przy ładowaniu strony
+    toggleFieldsForCompany($('input[type=radio][name="RegisterForm[isCompany]"]:checked').val());
+
+    $('input[type=radio][name="RegisterForm[isCompany]"]').change(function() {
+        toggleFieldsForCompany(this.value);
+    });
+
+    function toggleFieldsForCompany(value) {
+        // Definiowanie pól, które mają być ukryte dla Company
+        var fieldsToToggle = ['.field-registerform-birthdate',
+                              '.field-registerform-street',
+                              '.field-registerform-house_no',
+                              '.field-registerform-flat_no',
+                              '.field-registerform-postcode',
+                              '.field-registerform-city',
+							  '.field-registerform-country',
+                              '.field-registerform-voivodeship',
+							  '.field-registerform-id_document_no',
+                              '.field-registerform-pesel'];
+		
+        // Jeśli wybrana wartość to 1 (firma), ukryj pola
+        if(value == 1){
+            fieldsToToggle.forEach(function(field) {
+                $(field).addClass('hidden');
+            });
+            $('.companyFields').removeClass('hidden');
+        }else{
+            // W przeciwnym razie (osoba fizyczna), pokaż pola
+            fieldsToToggle.forEach(function(field) {
+                $(field).removeClass('hidden');
+            });
+            $('.companyFields').addClass('hidden');
+        }
+    }
+});
+</script>
 
 </div>
