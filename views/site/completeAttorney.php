576a3b890c25b566707994b4681b0c8d25fa7de4
galusmarcin87 <galusmarcin87@gmail.com>
2024-05-07 21:12:27 +0200

    1207238630160135

--- views/site/completeAttorney.php
+++ views/site/completeAttorney.php
@@ -17,28 +17,29 @@ $this->title = Yii::t('db', 'Complete the power of attorney');
 <div class="container">
 
     <div class="row">
+		<center><h3><?= Yii::t('db', 'Choose the method of signing documents') ?></h3></center><br><br><br><br>
         <div class="col-lg-6">
-            <h3><?= Yii::t('db', 'Electronic signature') ?></h3>
-            <p><?= Yii::t('db', 'Waiting time about 3 minutes') ?></p>
+            <center><h3><?= Yii::t('db', 'Electronic signature') ?></h3></center>
+            <center><p><?= Yii::t('db', 'Waiting time about 3 minutes') ?></p></center>
             <p>
-                <a class="btn btn-primary"
+                <center><a class="btn btn-primary"
                    href="<?= MgHelpers::getSetting('wzor pełnomocnictwa ' . Yii::$app->language) ?>">
                     <?= Yii::t('db', 'Power of attorney template') ?>
-                </a>
+                </a></center>
             </p>
-            <?= MgHelpers::getSetting('podpis elektroniczny text ' . Yii::$app->language, true, 'podpis elektroniczny text') ?>
+            <center><?= MgHelpers::getSetting('podpis elektroniczny text ' . Yii::$app->language, true, 'podpis elektroniczny text') ?></center>
 
         </div>
         <div class="col-lg-6">
-            <h3><?= Yii::t('db', 'Physical signature') ?></h3>
-            <p><?= Yii::t('db', 'Waiting time about 1 week') ?></p>
+            <center><h3><?= Yii::t('db', 'Physical signature') ?></h3></center>
+            <center><p><?= Yii::t('db', 'Waiting time about 1 week') ?></p></center>
             <p>
-                <a class="btn btn-primary"
+                <center><a class="btn btn-primary"
                    href="<?= MgHelpers::getSetting('wzor pełnomocnictwa ' . Yii::$app->language) ?>">
                     <?= Yii::t('db', 'Power of attorney template') ?>
-                </a>
+                </a></center>
             </p>
-            <?= MgHelpers::getSetting('podpis fizyczny text ' . Yii::$app->language, true, 'podpis fizyczny text') ?>
+            <center><?= MgHelpers::getSetting('podpis fizyczny text ' . Yii::$app->language, true, 'podpis fizyczny text') ?></center>
         </div>
     </div>
 
